b66054ac8be5e38c8cf2a92ddaf5fef8e5a02e46
Engel62 <sr.engel@bk.ru>
2022-10-25 14:03:12 +0300

    Домашнее задание 1, задачи 1-3 выпонены

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args){
+        int age = 19;
+        boolean canNotGoToTheSchool = age < 7 || age > 18;
+        if (age >= 18 && age < 21) {
+            System.out.println(" Ты достаточно взрослый, чтобы водить, но не достаточно, чтобы пить алкоголь");
+        }
+        if (canNotGoToTheSchool) {
+            System.out.println(" Я думаю ты не ходишь в школу");
+        }
 
     }
 }
