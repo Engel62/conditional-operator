7b50ae554b704dd4ac2a9bb949c6fc94f1fb84dd
Engel62 <sr.engel@bk.ru>
2022-10-25 13:42:07 +0300

    Домашнее задание 1, задачи 1-3 выпонены

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        // Задание 1
+        // Задание 1, домашнее задание 1
         System.out.println("Задание 1");
         int age = 20;
         if (age >= 18) {
@@ -42,6 +42,33 @@ public class Main {
         if (passenger >= wagonCapacity) {
             System.out.println("В вагоне нет мест");
         }
+        /// Задание 6, домашнее задание 2
+        System.out.println(" Домашнее задание 2. Задание 1");
+        if (age >= 18) {
+            System.out.println("Поздравляем с совершеннолетием!");
+        } else {
+            System.out.println("возраст совершеннолетия ещё не наступил, нужно немного подождать.");
+        }
+        /// Задание 2
+        System.out.println("Задание 2");
 
+        if (childAge >= 7 && age <= 18) {
+            System.out.println("Ребенок ходит в школу");
+        }
+        if (childAge >= 18 && childAge <= 24) {
+            System.out.println("человек уже закончил школу и может отправляться в университет");
+        } else {
+            System.out.println("Человек окончил университет и ему пора искать первую работу");
+        }
+        /// Задание 3
+        System.out.println("Задание 3");
+        if (passenger <= seatPlace) {
+            System.out.println("В вагоне есть сидячие места");
+        }
+        if (seatPlace <= passenger && passenger <= wagonCapacity) {
+            System.out.println("В вагоне есть стоячие места");
+        } else {
+            System.out.println("В вагоне нет мест");
+        }
     }
 }
